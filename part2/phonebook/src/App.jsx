7edac2de34b1f6fb0ab0f8cfdda40ad61f047831
jhmfreitas <jhmfreitas1998@gmail.com>
2025-08-22 13:20:44 +0100

    Adds axios usage to retrieve persons

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,19 +1,26 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 import Filter from './Filter.jsx'
 import PersonForm from './PersonForm.jsx'
 import Persons from './Persons.jsx'
 
 const App = () => {
- const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ])
+ const [persons, setPersons] = useState([])
 
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [newPersonToFind, setNewPersonToFind] = useState('')
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:3001/persons")
+      .then(response => {
+        console.log("Promise fullfiled")
+        console.log('Response retrieved: ',response)
+        setPersons(response.data)
+      })
+  }, [])
+  console.log('Rendered ', persons.length, ' persons')
 
   const addPerson = (event) => {
     event.preventDefault()
@@ -34,10 +41,6 @@ const App = () => {
     setNewName('')
     setNewNumber('')
   }
-
-
-
-
 
   return (
     <div>
